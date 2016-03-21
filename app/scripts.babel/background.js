a2a9f9be1097262cbdc4326ef6d0c5126b8ce8fa
Miguel Zaldivar <miguel.ezh@gmail.com>
2016-03-21 03:46:40 -0400

    commit

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,21 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+chrome.webNavigation.onCompleted.addListener(function(details){
+  chrome.tabs.executeScript(null, {file:'/bower_components/jquery/dist/jquery.min.js'}, function(){
+    chrome.tabs.executeScript(details.tabId, {
+      code: '(function() { var re = /\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\2([0-9]{4})/g;' +
+      `var listOfNumbers = document.body.innerText.match(re);
+
+       console.log(listOfNumbers);
+       listOfNumbers.forEach(function(item){
+          //$('a:contains(' + item + ')').each(function(item){
+          //    console.log($(item));
+          //});
+          $.each($('a:contains(' + item + ')'), function(item){
+            console.log(item);
+          });
+       });
+      })();`
+    });
+  });
 });
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
